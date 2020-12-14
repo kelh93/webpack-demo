fa8e0ae2897d7054b950f61f7f5e553998cb9c83
kelh93 <kelh93@163.com>
2020-12-14 19:22:10 +0800

    创建目录

---
+++ build_old/webpack.ssr.js
@@ -0,0 +1,57 @@
+const webpack = require('webpack');
+const HardSourceWebpackPlugin = require('hard-source-webpack-plugin'); //模块缓存
+const mergeWebpack = require('webpack-merge');
+const getWebpackBaseConfig = require('./webpack.base.js');
+const TerserPlugin = require('terser-webpack-plugin');
+// const HTMLInlineCSSWebpackPlugin = require('html-inline-css-webpack-plugin').default;
+const ManifestPlugin = require('webpack-manifest-plugin');
+const path = require('path');
+const MyCleanPlugin = require('./MyCleanPlugin');
+
+module.exports = function(buildConfig = {}) {
+    const useCache = !!buildConfig.useCache;
+	const useHash = !!buildConfig.useHash;
+    const plugins = [
+		new webpack.HotModuleReplacementPlugin(),
+		new ManifestPlugin({
+			generate: (seed, files, entrypoints) => {
+				var manifest = files.reduce(function (manifest, file) {
+					if (!manifest || !manifest[file.name]) {
+						manifest[file.name] = file.path;
+					}
+					return manifest;
+				}, seed);
+				return manifest;
+			}
+		}),
+		new MyCleanPlugin({
+			cleanDir: [path.join(buildConfig.rootDir, 'dist'), path.join(buildConfig.rootDir, 'app/public')],
+			srcDir: path.join(buildConfig.rootDir, 'dist'),
+			tarDir: path.join(buildConfig.rootDir, 'app/public'),
+		})
+	];
+    if (useCache) {
+        plugins.push(new HardSourceWebpackPlugin());
+    }
+
+    return mergeWebpack([getWebpackBaseConfig({ isSsr: true, buildConfig }), {
+        mode: 'production',
+        entry: buildConfig.ssrEntry,
+        output: {
+            publicPath: buildConfig.publicPath || '/',
+            path: buildConfig.distDir,
+            libraryTarget: 'umd',
+            filename: useHash ? '[name]-[hash:8].js': '[name].js?[hash:8]' //'[name]-[hash].js'
+        },
+        plugins,
+        optimization: {
+            minimize: true,
+            minimizer: [
+                new TerserPlugin({
+                    parallel: true,
+                    cache: useCache
+                })
+            ]
+        }
+    }]);
+};
---
+++ config/config.default.js
@@ -0,0 +1,6 @@
+module.export = (appInfo) => {
+  let config = {};
+  config.keys = "webpack-setting";
+  config.view = {};
+  return config;
+};
